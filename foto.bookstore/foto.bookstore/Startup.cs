259d0f9f05f04402650aaf8e48a6889ca88cfa38
AJAY SINGH YADAV <rawdy2012@gmail.com>
2021-03-27 16:58:13 +0530

    Added mvc to application

--- foto.bookstore/foto.bookstore/Startup.cs
+++ foto.bookstore/foto.bookstore/Startup.cs
@@ -16,6 +16,7 @@ namespace foto.bookstore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +26,55 @@ namespace foto.bookstore
             {
                 app.UseDeveloperExceptionPage();
             }
+            //app.Use(async(context, next) =>
+            //{
+            //    await context.Response.WriteAsync("hello from my first middleware");
+            //    await next();
+            //    await context.Response.WriteAsync("hello from my first middleware response");
+            //});
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("hello from my second middleware");
+            //    await next();
+            //    await context.Response.WriteAsync("hello from my second middleware response");
+            //});
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("hello from my third middleware");
+            //    await next();
+               
+            //});
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    if (env.IsDevelopment())
+                //    {
+                //        await context.Response.WriteAsync("Hello from dev");
+                //    }
+                //    else if (env.IsProduction())
+                //    {
+                //        await context.Response.WriteAsync("Hello from Prod");
+                //    }
+                //    else if (env.IsStaging())
+                //    {
+                //        await context.Response.WriteAsync("Hello from staging");
+                //    }
+                //    else
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.Map("/ajay", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello ajay from!");
+            //    });
+            //});
         }
     }
 }
