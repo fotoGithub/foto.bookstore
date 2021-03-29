30fde515f46a97ce3d04b781fc1e8b72848c50b5
AJAY SINGH YADAV <rawdy2012@gmail.com>
2021-03-29 16:34:58 +0530

    Added repository to application

---
+++ foto.bookstore/foto.bookstore/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using foto.bookstore.Models;
+using foto.bookstore.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace foto.bookstore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepository _bookRepository = null;
+        public BookController()
+        {
+            _bookRepository = new BookRepository();
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+            //return $"Book with id= {id} ";
+        }
+        public List<BookModel> SearchBooks(string bookName,string authorName)
+        {
+            return _bookRepository.SearchBook(bookName, authorName);
+            //return $"Book with name = {bookName} & author = {authorName}";
+        }
+    }
+}
---
+++ foto.bookstore/foto.bookstore/Repository/BookRepository.cs
@@ -0,0 +1,35 @@
+﻿using foto.bookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace foto.bookstore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSurce();
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSurce().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title, string authorName)
+        {
+            return DataSurce().Where(x => x.Title.Contains(title) && x.Author.Contains(authorName)).ToList();
+        }
+        private List<BookModel> DataSurce()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id=1,Title="MVC",Author="Ajay"},
+                new BookModel(){Id=2,Title="Java",Author="Priyanka"},
+                new BookModel(){Id=3,Title="HTML",Author="Rajan"},
+                new BookModel(){Id=4,Title="Angular",Author="Nikhil"},
+                new BookModel(){Id=5,Title="C-sharp",Author="Arihhant"},
+            };
+        }
+    }
+}
